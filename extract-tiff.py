656d386772e1146f4611552e29f7ac6e135d1d67
Christopher Tastad <christopher.tastad@mssm.edu>
2024-03-21 17:17:04 -0400

    semi complete test for high res processing

--- extract-tiff.py
+++ extract-tiff.py
@@ -4,9 +4,9 @@ import tifffile
 
 # Variable 'LEVEL' determines the level to extract. It ranges from 0 (highest
 # resolution) to 6 (lowest resolution) for morphology.ome.tif
-LEVEL = 2
+LEVEL = 1
 
-with tifffile.TiffFile('/home/cw/Downloads/output-XETG00189__0010700__50452B-TUQ97N-EA__20240126__205019/morphology.ome.tif') as tif:
+with tifffile.TiffFile('/sc/arion/projects/untreatedIBD/chobiolab-core/shared-xenium-library/outputs/TUQ97N/CHO-001/output-XETG00189__0010700__50452B-TUQ97N-EA__20240126__205019/morphology.ome.tif') as tif:
     image = tif.series[0].levels[LEVEL].asarray()
 
 tifffile.imwrite(
--- run-cellpose.sh
+++ run-cellpose.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-python -m cellpose \
-  --dir /home/cw/projects/cholab/segpro \
+cellpose \
+  --dir /sc/arion/projects/untreatedIBD/ctastad/projects/segpro \
   --pretrained_model nuclei \
   --chan 0 \
   --chan2 0 \
   --img_filter _morphology.ome \
-  --diameter 0.85 \
+  --diameter 10 \
   --do_3D \
   --save_tif \
   --verbose \
   --use_gpu
+  #--batch_size 1 \
